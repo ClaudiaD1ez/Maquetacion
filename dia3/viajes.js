899fd70280fe9ec3d383e535b39388bc7318a65b
Claudia Diez <claudia_diez@outlook.com>
2022-05-24 11:47:45 +0200

    correccionJS

--- dia3/viajes.js
+++ dia3/viajes.js
@@ -1,23 +1,5 @@
-// document.getElementById("nombre")
-
-// document.getElementById("origen")
-
-// document.getElementById("destino")
-
-// document.getElementById("persona")
-
-// document.getElementById("fecha1")
-
-// document.getElementById("fecha2")
-
-// document.getElementById("recoger"){
-
-// }
 
 // document.getElementById("filtrar")
-
-
-
 // createElement()
 // appendChild()
 
@@ -42,23 +24,22 @@ function recogerDatos(){
 
 
 function filtrar(){
-    // let imprimir = "";
-   for(i=0; i<bbdd.length;i++){
+    let imprimir = []
+   for(let i=0; i<bbdd.length;i++){
       if(bbdd[i].destino == "CANARIAS" || bbdd[i].destino == "MALLORCA" || bbdd[i].destino == "GALICIA"){
         console.log(bbdd[i]);
+        imprimir.push(bbdd[i]);
         // imprimir += bbdd[i].nombre
         // for(j=0 ; j<bbdd[i]; j++){
         //     imprimir += bbdd[i][j]
         // }
-
-        document.getElementById("impNombre").innerHTML =  bbdd[i].nombre
-        document.getElementById("impOrigen").innerHTML = bbdd[i].origen
-        document.getElementById("impDestino").innerHTML = bbdd[i].destino
-        document.getElementById("impPersonas").innerHTML = bbdd[i].personas
-        document.getElementById("impFechas").innerHTML = bbdd[i].fecha1
       }
-
    }
-   
-        
+   for(let j=0; j<imprimir.length;j++){
+        document.getElementById("impNombre").innerHTML +=  imprimir[j].nombre + " | "
+        document.getElementById("impOrigen").innerHTML += imprimir[j].origen + "| "
+        document.getElementById("impDestino").innerHTML += imprimir[j].destino + " | "
+        document.getElementById("impPersonas").innerHTML += imprimir[j].personas + " | "
+        document.getElementById("impFechas").innerHTML += imprimir[j].fecha1 + " | "
+      }
 }
